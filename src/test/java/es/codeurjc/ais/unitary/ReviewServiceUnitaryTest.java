cecae9fe11b2355fa59da37f5e8195068877095d
Laara2705 <127790380+Laara2705@users.noreply.github.com>
2023-05-03 12:38:39 +0200

    Update ReviewServiceUnitaryTest.java

--- src/test/java/es/codeurjc/ais/unitary/ReviewServiceUnitaryTest.java
+++ src/test/java/es/codeurjc/ais/unitary/ReviewServiceUnitaryTest.java
@@ -1,43 +1,88 @@
 package es.codeurjc.ais.unitary;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.util.Arrays;
-
+import es.codeurjc.ais.book.BookDetail;
+import es.codeurjc.ais.book.BookService;
+import es.codeurjc.ais.book.OpenLibraryService;
+import es.codeurjc.ais.notification.NotificationService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.HttpClientErrorException;
+import java.util.Arrays;
+import java.util.Optional;
 
-import es.codeurjc.ais.notification.NotificationService;
-import es.codeurjc.ais.review.Review;
-import es.codeurjc.ais.review.ReviewRepository;
-import es.codeurjc.ais.review.ReviewService;
-
-@DisplayName("BookService Unitary tests")
+import static org.mockito.Mockito.*;
+/**
+ * This class contains all tests about unitary testing
+ * Proving the logic of a component
+ * @author Diego Picazo, Lara Fernandez
+ * @since 2023-03-07
+ */
+@DisplayName("Testing BookService class ...")
 public class ReviewServiceUnitaryTest {
-
-    @Test
-    public void getReviewsByIdUnitTest(){
-
-        Review review1 = new Review();
-        review1.setBookId("123A");
-        review1.setNickname("nickname1");
-        review1.setContent("content1");
-
-        Review review2 = new Review();
-        review2.setBookId("123A");
-        review2.setNickname("nickname2");
-        review2.setContent("content2");
-
-        ReviewRepository reviewRepository = mock(ReviewRepository.class);
-        when(reviewRepository.findByBookId("123A")).thenReturn(Arrays.asList(review1, review2));
-        NotificationService notificationService = mock(NotificationService.class);
-        ReviewService reviewService = new ReviewService(reviewRepository, notificationService);
-
-        assertEquals(2, reviewService.findByBookId("123A").size());
-
-
-    }
-
+	static BookService bookService = null;
+	static OpenLibraryService openLibraryService = null;
+	static NotificationService notificationService = null;
+	static String success;
+	static String fail;
+	@BeforeAll
+	public static void setUp(){
+		// Instance used for both Tests
+		bookService = new BookService((openLibraryService = mock(OpenLibraryService.class)),
+				(notificationService = mock(NotificationService.class)));
+		success = "NotificationService: SUCCESS!";
+		fail = "NotificationService: FAILED!";
+	}
+	@Test
+	@DisplayName("Got 2 magic books & notification served")
+	public void testUNIT_Magic(){
+		/* Given */
+		int magic_books;
+		final int NUM_BOOKS = 2;
+		/* When */
+		when(openLibraryService.searchBooks("magic", 10))
+				.thenReturn(Arrays.asList(
+						new OpenLibraryService.BookData(null, "/work/01", null, null, null),
+						new OpenLibraryService.BookData(null, "/work/02", null, null, null)));
+		magic_books = bookService.findAll("magic").size();
+		notificationService.info(success);
+		/* Then */
+		Assertions.assertEquals(NUM_BOOKS, magic_books);
+		// Verify success message
+		verify(notificationService).info(success);
+	}
+	@Test
+	@DisplayName("Got null book & notification error served")
+	public void testUNIT_NonRecover(){
+		/* Given */
+		Optional<BookDetail> bookDetailFail;
+		/* When */
+		// invalid id
+		when(openLibraryService.getBook("anyString()"))
+				.thenThrow(new HttpClientErrorException(HttpStatusCode.valueOf(404)));
+		bookDetailFail = bookService.findById("anyString()");
+		notificationService.error(fail);
+		/* Then */
+		Assertions.assertSame(Optional.empty(), bookDetailFail,
+				"bookDetail should be empty");
+		// Verify error message
+		verify(notificationService).error(fail);
+	}
+	@Test
+	@DisplayName("EXTRA: Checking good ID-book passed")
+	// EXTRA test checking what happens if a good ID is passed
+	public void testUNIT_EXTRA(){
+		/* Given */
+		Optional<BookDetail> bookDetailGood;
+		// random valid id of an existing book
+		when(openLibraryService.getBook("OL259010W"))
+				.thenReturn(
+						new OpenLibraryService.BookData(null, "/work/01",
+								null, new Integer[1], new String[]{"HELLO WORLD"}));
+		bookDetailGood = bookService.findById("OL259010W");
+		Assertions.assertNotSame(Optional.empty(), bookDetailGood,
+				"bookDetail should not be empty");
+	}
 }
